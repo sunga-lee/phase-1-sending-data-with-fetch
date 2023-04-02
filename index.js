edfc301395267cb15c8f341e500a42a84abbeaf7
sunga-lee <sungajunior1@gmail.com>
2023-04-02 22:45:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+const formData = {
+    name: "Sunga",
+    email: "sungajunior1@gmail.com",
+  };
+   
+const  submitData = {
+  method:"POST",
+  headers: {
+    "Content-Type":"application/json",
+    "Accept":"application/json",
+
+  },
+  body:JSON.stringify({
+    name: "Sunga",
+    email: "sungajunior1@gmail.com",
+  }),
+
+};
+
+fetch("http://localhost:3000/users ", submitData);
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+  .catch(function (error) {
+    alert("error occured!, oblect with error!");
+    console.log(error.message);
+  });
